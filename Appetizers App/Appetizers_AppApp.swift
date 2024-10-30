2c3f3aa6314d788feb8908721d5dfe461006e759
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-10-29 18:51:38 -0700

    Initial Commit

---
+++ Appetizers App/Appetizers_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appetizers_AppApp.swift
+//  Appetizers App
+//
+//  Created by Nastya Klyashtorna on 2024-10-29.
+//
+
+import SwiftUI
+
+@main
+struct Appetizers_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appetizers App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Appetizers App
+//
+//  Created by Nastya Klyashtorna on 2024-10-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
