6e258574b71c860a43ea750066b5900b749a983a
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-10-31 22:04:46 -0700

    Add request Errors anum & getData function

---
+++ Appetizers App/ApError.swift
@@ -0,0 +1,15 @@
+//
+//  ApError.swift
+//  Appetizers App
+//
+//  Created by Nastya Klyashtorna on 2024-10-31.
+//
+
+import Foundation
+
+enum ApError: Error {
+    case invalidURL
+    case invalidResponce
+    case invalidData
+    case unableToCompleteRequest
+}
---
+++ Appetizers App/NetworkManager.swift
@@ -0,0 +1,52 @@
+//
+//  NetworkManager.swift
+//  Appetizers App
+//
+//  Created by Nastya Klyashtorna on 2024-10-31.
+//
+import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    static let baseUrl: String = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals"
+    private let appetizerUrl = baseUrl + "/appetizers"
+    private init() {
+        
+    }
+    
+    func getAppetizers(completion: @escaping (Result<[Appetizer], ApError>) -> Void) {
+        guard let url = URL(string: appetizerUrl) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        let task = URLSession.shared.dataTask(with: URLRequest(url : url)) { data, responce, error in
+            guard let _ = error else {
+                completion(.failure(.unableToCompleteRequest))
+                return
+            }
+            
+            guard let responce = responce as? HTTPURLResponse, responce.statusCode == 200 else {
+                completion(.failure(.invalidResponce))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let decoded = try decoder.decode(ResponceData.self, from: data)
+                completion(.success(decoded.request))
+            } catch {
+                completion(.failure(.invalidData))
+            }
+        }
+        
+        task.resume()
+
+    }
+}
+
