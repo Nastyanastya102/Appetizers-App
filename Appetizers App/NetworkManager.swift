29a7686017f489d9b0aae218df1b6ccbfc07dd60
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-11-01 23:23:28 -0700

    Add image download func & caching

--- Appetizers App/NetworkManager.swift
+++ Appetizers App/NetworkManager.swift
@@ -5,9 +5,12 @@
 //  Created by Nastya Klyashtorna on 2024-10-31.
 //
 import Foundation
+import SwiftUI
 
 final class NetworkManagerAPI {
     static let shared = NetworkManagerAPI()
+    
+    private var cache = NSCache<NSString, UIImage>()
     
     static let baseUrl = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals"
     private let appetizerUrl = baseUrl + "/appetizers"
@@ -46,7 +49,31 @@ final class NetworkManagerAPI {
         }
 
         task.resume()
-
+    }
+    
+    func downloadImage(urlString: String, completed: @escaping (UIImage?) -> Void ) {
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, responce, error in
+            guard let data = data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
+        }
+        
+        task.resume()
     }
 }
 
